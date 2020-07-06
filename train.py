fbcef5da7a8300dadb258f25243fe9ec90be5bf8
Vladimir Lyashenko <v.liashenko@neurodatalab.com>
2020-07-06 19:12:11 +0300

    Updated train.py Affectnet_loader.py

--- train.py
+++ train.py
@@ -15,175 +15,187 @@ import time
 import numpy as np
 import scipy.io
 from sklearn.metrics import accuracy_score
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
 
-# gpu init
-gpu_list = ''
-multi_gpus = False
-if isinstance(GPU, int):
-    gpu_list = str(GPU)
-else:
-    multi_gpus = True
-    for i, gpu_id in enumerate(GPU):
-        gpu_list += str(gpu_id)
-        if i != len(GPU) - 1:
-            gpu_list += ','
-os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
 
-# other init
-start_epoch = 1
-save_dir = os.path.join(SAVE_DIR, MODEL_PRE + 'v2_' + datetime.now().strftime('%Y%m%d_%H%M%S'))
-if os.path.exists(save_dir):
-    raise NameError('model dir exists!')
-os.makedirs(save_dir)
-logging = init_log(save_dir)
-_print = logging.info
+
+# def count_loss(raw_logits, label):
+#     loss = 0
+#     weights_for_loss = [0.26373185113172853,
+#                     0.47345915139731315,
+#                     0.0896753104944664,
+#                     0.04962980183302689,
+#                     0.022465498657987616,
+#                     0.013395467450035576,
+#                     0.0876429190354418]
+#     for i in range(raw_logits.shape[1]):
+#         loss += weights_for_loss[i]*nn.BCELoss(raw_logits[:,i], label[:,i])
+#     return loss
+
+def aug(p=0.5):
+    return transforms.Compose([
+    transforms.ToPILImage(), 
+    transforms.RandomHorizontalFlip(p = p),
+    transforms.ToTensor()
+])
+
+
+# writer = SummaryWriter()
+
+# # gpu init
+# gpu_list = ''
+# multi_gpus = False
+# if isinstance(GPU, int):
+#     gpu_list = str(GPU)
+# else:
+#     multi_gpus = True
+#     for i, gpu_id in enumerate(GPU):
+#         gpu_list += str(gpu_id)
+#         if i != len(GPU) - 1:
+#             gpu_list += ','
+# os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+
+# # other init
+# start_epoch = 1
+# save_dir = os.path.join(SAVE_DIR, MODEL_PRE + 'v2_' + datetime.now().strftime('%Y%m%d_%H%M%S'))
+# if os.path.exists(save_dir):
+#     raise NameError('model dir exists!')
+# os.makedirs(save_dir)
+# logging = init_log(save_dir)
+# _print = logging.info
 
 
 # define trainloader and testloader
-trainset = Affectnet_aligned()
+
+
+trainset = Affectnet_aligned(transform = aug())
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
                                           shuffle=True, num_workers=8, drop_last=False)
+for data in trainloader:
+    img, label = data[0].cuda(), data[1].cuda()
+    print(label)
 
-# nl: left_image_path
-# nr: right_image_path
-# nl, nr, folds, flags = parseList(root=LFW_DATA_DIR)
-testdataset = Affectnet_test_aligned()
-testloader = torch.utils.data.DataLoader(testdataset, batch_size=32,
-                                         shuffle=False, num_workers=8, drop_last=False)
+    true_label = []
+    for i in label.cpu().detach():
+        l = []
+        for j in range(7):
+            if i == j:
+                l.append(1)
+            else:
+                l.append(0)
+        true_label.append(l)
+    print(true_label[:,0])
+    break
 
-# define model
-net = model_new.MobileFacenet()
-# ArcMargin = model.ArcMarginProduct(128, trainset.class_nums)
+# testdataset = Affectnet_test_aligned()
+# testloader = torch.utils.data.DataLoader(testdataset, batch_size=32,
+#                                          shuffle=False, num_workers=8, drop_last=False)
 
-if RESUME:
-    ckpt = torch.load(RESUME)
-    net.load_state_dict(ckpt['net_state_dict'])
-    start_epoch = ckpt['epoch'] + 1
+# # define model
+# net = model_new.MobileFacenet()
+# # ArcMargin = model.ArcMarginProduct(128, trainset.class_nums)
 
-
-# define optimizers
-ignored_params = list(map(id, net.dense2.parameters()))
-# ignored_params += list(map(id, ArcMargin.weight))
-prelu_params_id = []
-prelu_params = []
-for m in net.modules():
-    if isinstance(m, nn.PReLU):
-        ignored_params += list(map(id, m.parameters()))
-        prelu_params += m.parameters()
-base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
-
-optimizer_ft = optim.SGD([
-    {'params': base_params, 'weight_decay': 4e-5},
-    {'params': net.dense2.parameters(), 'weight_decay': 4e-4},
-    #{'params': ArcMargin.weight, 'weight_decay': 4e-4},
-    {'params': prelu_params, 'weight_decay': 0.0}
-], lr=0.1, momentum=0.9, nesterov=True)
-
-exp_lr_scheduler = lr_scheduler.MultiStepLR(optimizer_ft, milestones=[36, 52, 58], gamma=0.1)
+# if RESUME:
+#     ckpt = torch.load(RESUME)
+#     net.load_state_dict(ckpt['net_state_dict'])
+#     start_epoch = ckpt['epoch'] + 1
 
 
-net = net.cuda()
-# ArcMargin = ArcMargin.cuda()
-if multi_gpus:
-    net = DataParallel(net)
-    # ArcMargin = DataParallel(ArcMargin)
-criterion = torch.nn.CrossEntropyLoss()
+# # define optimizers
+# ignored_params = list(map(id, net.dense2.parameters()))
+# prelu_params_id = []
+# prelu_params = []
+# for m in net.modules():
+#     if isinstance(m, nn.PReLU):
+#         ignored_params += list(map(id, m.parameters()))
+#         prelu_params += m.parameters()
+# base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
+
+# optimizer_ft = optim.SGD([
+#     {'params': base_params, 'weight_decay': 4e-5},
+#     {'params': net.dense1.parameters(), 'weight_decay': 4e-4},
+#     #{'params': ArcMargin.weight, 'weight_decay': 4e-4},
+#     {'params': prelu_params, 'weight_decay': 0.0}
+# ], lr=0.1, momentum=0.9, nesterov=True)
+
+# exp_lr_scheduler = lr_scheduler.MultiStepLR(optimizer_ft, milestones=[36, 52, 58], gamma=0.1)
 
 
-best_acc = 0.0
-best_epoch = 0
-for epoch in range(start_epoch, TOTAL_EPOCH+1):
-    exp_lr_scheduler.step()
-    # train model
-    _print('Train Epoch: {}/{} ...'.format(epoch, TOTAL_EPOCH))
-    net.train()
+# net = net.cuda()
+# if multi_gpus:
+#     net = DataParallel(net)
 
-    train_total_loss = 0.0
-    total = 0
-    since = time.time()
-    for data in trainloader:
-        img, label = data[0].cuda(), data[1].cuda()
-        batch_size = img.size(0)
-        optimizer_ft.zero_grad()
 
-        raw_logits = net(img)
+# best_acc = 0.0
+# best_epoch = 0
+# for epoch in range(start_epoch, TOTAL_EPOCH+1):
+#     exp_lr_scheduler.step()
+#     # train model
+#     _print('Train Epoch: {}/{} ...'.format(epoch, TOTAL_EPOCH))
+#     net.train()
 
-        # output = ArcMargin(raw_logits, label)
-        # print(raw_logits)
-        # print(label)
-        total_loss = criterion(raw_logits, label)
+#     train_total_loss = 0.0
+#     total = 0
+#     since = time.time()
+#     for data in trainloader:
+#         img, label = data[0].cuda(), data[1].cuda()
+#         batch_size = img.size(0)
+#         optimizer_ft.zero_grad()
 
-        total_loss.backward()
-        optimizer_ft.step()
-        # print(total_loss)
+#         raw_logits = net(img)
 
-        # print(train_total_loss)
-        train_total_loss += total_loss.item() * batch_size
-        total += batch_size
+#         total_loss = count_loss(raw_logits, label)
 
-    train_total_loss = train_total_loss / total
-    time_elapsed = time.time() - since
-    loss_msg = '    total_loss: {:.4f} time: {:.0f}m {:.0f}s'\
-        .format(train_total_loss, time_elapsed // 60, time_elapsed % 60)
-    _print(loss_msg)
+#         total_loss.backward()
+#         optimizer_ft.step()
+#         # print(total_loss)
 
-    # # test model on lfw
-    if epoch % TEST_FREQ == 0:
-        net.eval()
-        featureLs = None
-        featureRs = None
-        _print('Test Epoch: {} ...'.format(epoch))
-        ma = []
-        for data in testloader:
-            img, label = data[0].cuda(), data[1].cuda()
-            batch_size = img.size(0)
-            raw_logits = net(img)
+#         # print(train_total_loss)
+#         train_total_loss += total_loss * batch_size
+#         writer.add_scalar('Loss/train', train_total_loss, epoch)
+#         total += batch_size
+
+#     train_total_loss = train_total_loss / total
+#     time_elapsed = time.time() - since
+#     loss_msg = '    total_loss: {:.4f} time: {:.0f}m {:.0f}s'\
+#         .format(train_total_loss, time_elapsed // 60, time_elapsed % 60)
+#     _print(loss_msg)
+
+#     # # test model on Affectnet validation set
+#     if epoch % TEST_FREQ == 0:
+#         net.eval()
+#         _print('Test Epoch: {} ...'.format(epoch))
+#         ma = []
+#         for data in testloader:
+#             img, label = data[0].cuda(), data[1].cuda()
+#             batch_size = img.size(0)
+#             raw_logits = net(img)
             
-            pred = []
-            for l in raw_logits.cpu().detach().numpy():
-                m = max(l)
-                for i in range(len(l)):
-                    if l[i] == m:
-                        pred.append(i)
-                        break
-            print(pred)
-            print('Accuracy: ', accuracy_score(label.cpu().detach().numpy(), pred))
-            ma.append(accuracy_score(label.cpu().detach().numpy(), pred))
-        print('Mean accuracy:', np.mean(ma))
+#             test_loss = count_loss(raw_logits, label)
+#             writer.add_scalar('Loss/test', test_loss, epoch)
+#             pred = []
+#             for l in raw_logits.cpu().detach().numpy():
+#                 m = max(l)
+#                 for i in range(len(l)):
+#                     if l[i] == m:
+#                         pred.append(i)
+#                         break
+#             ma.append(accuracy_score(label.cpu().detach().numpy(), pred))
+#         writer.add_scalar('Accuracy/test', np.mean(ma), epoch)
 
-            # for i in range(len(data)):
-            #     data[i] = data[i].cuda()
-
-        #     res = [net(d).data.cpu().numpy() for d in data]
-        #     featureL = np.concatenate((res[0], res[1]), 1)
-        #     featureR = np.concatenate((res[2], res[3]), 1)
-        #     if featureLs is None:
-        #         featureLs = featureL
-        #     else:
-        #         featureLs = np.concatenate((featureLs, featureL), 0)
-        #     if featureRs is None:
-        #         featureRs = featureR
-        #     else:
-        #         featureRs = np.concatenate((featureRs, featureR), 0)
-
-        # result = {'fl': featureLs, 'fr': featureRs, 'fold': folds, 'flag': flags}
-        # # save tmp_result
-        # scipy.io.savemat('./result/tmp_result.mat', result)
-        # accs = evaluation_10_fold('./result/tmp_result.mat')
-        # _print('    ave: {:.4f}'.format(np.mean(accs) * 100))
-
-    # save model
-    if epoch % SAVE_FREQ == 0:
-        msg = 'Saving checkpoint: {}'.format(epoch)
-        _print(msg)
-        if multi_gpus:
-            net_state_dict = net.module.state_dict()
-        else:
-            net_state_dict = net.state_dict()
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
-        torch.save({
-            'epoch': epoch,
-            'net_state_dict': net_state_dict},
-            os.path.join(save_dir, '%03d.ckpt' % epoch))
-print('finishing training')
+#     # save model
+#     if epoch % SAVE_FREQ == 0:
+#         msg = 'Saving checkpoint: {}'.format(epoch)
+#         _print(msg)
+#         if multi_gpus:
+#             net_state_dict = net.module.state_dict()
+#         else:
+#             net_state_dict = net.state_dict()
+#         if not os.path.exists(save_dir):
+#             os.mkdir(save_dir)
+#         torch.save({
+#             'epoch': epoch,
+#             'net_state_dict': net_state_dict},
+#             os.path.join(save_dir, '%03d.ckpt' % epoch))
+# print('finishing training')
